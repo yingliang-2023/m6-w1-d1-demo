56236fd6a5548cd096af045c853dc051fef8370e
YING LIANG <y.liang.168@westcliff.edu>
2023-06-04 13:37:06 -0500

    init

---
+++ factory/gadgetFactory.js
@@ -0,0 +1,6 @@
+const Laptop = require("./laptop");
+const Tablet = require("./tablet");
+
+const gadget = { Laptop, Tablet };
+
+// add module.export here
---
+++ factory/index.js
@@ -0,0 +1,16 @@
+const gadgetFactory = require("./gadgetFactory");
+
+const myLaptop = gadgetFactory.createGadget("Laptop", {
+  ram: 8,
+  ssd: 256,
+  name: "Bab's MacBook Pro"
+});
+
+const myTablet = gadgetFactory.createGadget("Tablet", {
+  ram: 4,
+  hdd: 128,
+  network: "4G",
+  name: "Bab's iPad"
+});
+
+// add console.log here
